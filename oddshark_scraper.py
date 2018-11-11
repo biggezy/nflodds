ddcfc6bbbb357edbfaf0f64a1ab09cd104bf3445
Eric Tran <eric1tran@gmail.com>
2018-11-11 01:09:54 -0600

    Filter only over/under trends

--- oddshark_scraper.py
+++ oddshark_scraper.py
@@ -1,3 +1,4 @@
+import re
 import requests
 import json
 from bs4 import BeautifulSoup
@@ -22,12 +23,29 @@ links = [
 ]
 
 for t in links:
-    print("{} - {} vs {} => {}".format(t[0],t[1],t[2],t[3]))
+  if "2018-11-11" in t[0]:
+    print("#########################################")   
+    print("{} - {} vs {} => {}\n".format(t[0],t[1],t[2],t[3]))
+    awayteam = t[1]
+    hometeam= t[2]
     r = requests.get(t[3])
     soup = BeautifulSoup(r.content, "lxml")
     trends = [
         json.loads(v.text)
         for v in soup.findAll('script', {"type":"application/json", "id":"gc-data"})
     ]
-    print(trends[0]["oddsshark_gamecenter"]["trends"])
-    print("#########################################")
+
+    awayTrends = trends[0]["oddsshark_gamecenter"]["trends"]["away"]
+    print (awayteam, "(AWAY) Trends")
+    for rawTrend in awayTrends:
+      parsedTrend = rawTrend["value"]
+      if re.search("over|under", parsedTrend, re.IGNORECASE):
+        print (parsedTrend)
+
+
+    homeTrends = trends[0]["oddsshark_gamecenter"]["trends"]["home"]
+    print (hometeam, "(HOME) Trends")
+    for rawTrend in homeTrends:
+      parsedTrend = rawTrend["value"]
+      if re.search("over|under", parsedTrend, re.IGNORECASE):
+        print (parsedTrend)
